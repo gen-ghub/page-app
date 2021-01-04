c0f725a11f793163d54aa3b09b1bc9673c9b77b4
gen-ghub <renbenzhuang234@gmail.com>
2021-01-04 22:12:24 +0900

    トップページ基盤

--- components/Work.vue
+++ components/Work.vue
@@ -1,16 +1,86 @@
 <template>
-  <div class="grid grid-cols-2">
-    <section class="col-start-1 h-screen bg-green-400">
-
-    </section>
-    <div class="col-start-2 h-screen bg-green-200">
-
+  <div>
+    <div class="grid grid-cols-2">
+      <section class="col-start-1 h-screen bg-gray-200 flex justify-center items-center">
+        <div>
+          <h2>Be free within constraints</h2>
+          <h1>ACTRESS</h1>
+          <p>
+            「自分のこと」は自分が一番わからなかったりする。<br>
+            わからないことは、ワクワクする。<br>
+            積み重ねてきた過去の経験。感情の昇華。<br>
+            DETOXでもあり、新しい自分との出逢いである<br>
+            俳優という仕事を通して自分自身と向き合い<br>
+            宝物を探しながら、人生をおもしろく、豊かにしていく。</p>
+          <p>その人の魅力は「その人らしさ」<br>
+            唯一無二。
+          </p>
+          <p>
+            誰かの「らしさ」-生き方を変えるかもしれない<br>
+            魅力的な作品、世界の中を生きることで伝えていく。
+          </p>
+          <nuxt-link to="/Contact">
+            <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">Contact</button>
+          </nuxt-link>
+        </div>
+      </section>
+      <div class="col-start-2 h-screen bg-green-200">
+        <img class="h-screen w-screen object-cover" src="~/assets/leg.jpg" />
+      </div>
+      <div class="col-start-1 h-screen bg-green-200">
+        <img class="h-screen w-screen object-cover" src="~/assets/sun.jpg" />
+      </div>
+      <section class="col-start-2 h-screen bg-gray-200 flex justify-center items-center">
+        <div>
+          <h2>Happiness comes from within</h2>
+          <h1>COUNSELOR</h1>
+          <p>
+            「自分のこと」は自分が一番わからなかったりする。<br>
+            わからないことは、ワクワクする。<br>
+            積み重ねてきた過去の経験。感情の昇華。<br>
+            DETOXでもあり、新しい自分との出逢いである<br>
+            俳優という仕事を通して自分自身と向き合い<br>
+            宝物を探しながら、人生をおもしろく、豊かにしていく。</p>
+          <p>その人の魅力は「その人らしさ」<br>
+            唯一無二。
+          </p>
+          <p>
+            誰かの「らしさ」-生き方を変えるかもしれない<br>
+            魅力的な作品、世界の中を生きることで伝えていく。
+          </p>
+          <nuxt-link to="/Link01">
+            <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">service</button>
+          </nuxt-link>
+        </div>
+      </section>
+      <section class="col-start-1 h-screen bg-gray-200 flex justify-center items-center">
+        <div>
+          <h2>名前</h2>
+          <h1>name</h1>
+          <div>
+            <nuxt-link to="/Contact">
+              <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">profile</button>
+            </nuxt-link>
+            <nuxt-link to="/Contact">
+              <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">biography</button>
+            </nuxt-link>
+          </div>
+        </div>
+      </section>
+      <div class="col-start-2 h-screen bg-green-200">
+        <img class="h-screen w-screen object-cover" src="~/assets/woman2.jpg" />
+      </div>
     </div>
-    <section class="col-start-1 h-screen bg-green-200">
-
-    </section>
-    <div class="col-start-2 h-screen bg-green-400">
-
+    <div class="flex justify-center items-center h-screen">
+      <div>
+        <h2>Just the way you are</h2>
+        <p>
+          自分らしくのびのびと<br>
+          表現することをやめず<br>
+          輝くその才能が<br>
+          後世に引き継がれますように
+        </p>
+      </div>
     </div>
   </div>
 </template>
