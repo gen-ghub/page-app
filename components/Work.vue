310dff4810793b7ee2d95dd8102bbddbec0a75e6
gen-ghub <renbenzhuang234@gmail.com>
2021-01-04 20:23:16 +0900

    Work基盤

---
+++ components/Work.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid grid-cols-2">
+    <section class="col-start-1 h-screen bg-green-400">
+
+    </section>
+    <div class="col-start-2 h-screen bg-green-200">
+
+    </div>
+    <section class="col-start-1 h-screen bg-green-200">
+
+    </section>
+    <div class="col-start-2 h-screen bg-green-400">
+
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <section>
     <TopImage />
     <News />
+    <Work />
   </section>
 </template>
 
 <script>
 import TopImage from '@/components/TopImage.vue'
 import News from '@/components/News.vue'
+import Work from '@/components/Work.vue'
 
 export default {
   components: {
     TopImage,
-    News
+    News,
+    Work
   },
 }
 
