2a4113cacc808b5aab5d56626fce5c519ed55320
gen-ghub <renbenzhuang234@gmail.com>
2021-01-05 13:03:32 +0900

    トップページ修正

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,29 +1,38 @@
 <template>
-    <footer class="bg-black">
-      <div class="grid grid-cols-6 gap-4 pt-20 pb-10 ">
-        <div class="col-start-2 col-span-4 flex justify-evenly">
-          <!-- <div>
-            <ul>
-              <li><router-link to="/">トップページ</router-link></li>
-              <li><nuxt-link class="tracking-widest font-thin " to="/Link01">事業内容</nuxt-link></li>
-              <li><nuxt-link class="tracking-widest font-thin " to="/Link02">企業情報</nuxt-link></li>
-              <li><nuxt-link class="tracking-widest font-thin " to="/Contact">お問い合わせ</nuxt-link></li>
-            </ul>
-          </div> -->
-        </div>
+  <footer class="bg-black">
+    <div class="grid grid-cols-6 text-white py-16">
+      <div class="col-start-2 col-span-4 flex justify-evenly">
+        <section>
+          <h1 class="text-xl font-thin tracking-widest mb-4">COMPANY</h1>
+          <ul class="text-xs leading-relaxed">
+            <li>会社名</li>
+            <li>東京都港区南青山5丁目11-24 グレイセス2F</li>
+            <li>代表 山田 華子</li>
+            <li>設立 2018年3月1日</li>
+          </ul>
+        </section>
+        <section>
+          <h1 class="text-xl font-thin tracking-widest">SNS</h1>
+          <section class="flex">
+            <a href="/"><img class="px-2 py-1" src="~/assets/f.png" /></a><span>|</span>
+            <a href="/"><img class="px-2 py-1" src="~/assets/tw.png" /></a><span>|</span>
+            <a href="/"><img class="px-2 py-1" src="~/assets/in.png" /></a>
+          </section>
+        </section>
       </div>
-        <div class="flex justify-center text-white bg-black bg-opacity-50 py-6">
-          <p class="copyright font-thin text-sm">&copy; 2021 </p>
-        </div>
-    </footer>
+    </div>
+    <div class="copy-ber flex justify-center text-white py-6">
+      <p class="copyright font-thin text-sm">&copy; 2021 YAMADA HANAKO.</p>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-};
+export default {}
 </script>
 
 <style lang="scss" scoped>
-
+.copy-ber {
+  background-color: rgba(#1b1b1b, 1);
+}
 </style>
--- components/Work.vue
+++ components/Work.vue
@@ -1,89 +1,125 @@
 <template>
   <div>
-    <div class="grid grid-cols-2">
-      <section class="col-start-1 h-screen bg-gray-200 flex justify-center items-center">
-        <div>
-          <h2>Be free within constraints</h2>
-          <h1>ACTRESS</h1>
-          <p>
-            「自分のこと」は自分が一番わからなかったりする。<br>
-            わからないことは、ワクワクする。<br>
-            積み重ねてきた過去の経験。感情の昇華。<br>
-            DETOXでもあり、新しい自分との出逢いである<br>
-            俳優という仕事を通して自分自身と向き合い<br>
-            宝物を探しながら、人生をおもしろく、豊かにしていく。</p>
-          <p>その人の魅力は「その人らしさ」<br>
-            唯一無二。
-          </p>
-          <p>
-            誰かの「らしさ」-生き方を変えるかもしれない<br>
-            魅力的な作品、世界の中を生きることで伝えていく。
-          </p>
-          <nuxt-link to="/Contact">
-            <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">Contact</button>
+    <div class="grid grid-cols-2 h-full">
+      <section class="col-span-2 lg:col-span-1 lg:col-start-1 bg-gray-200 flex justify-center">
+        <div class="flex flex-col items-center">
+          <h2 class="text-yellow-600 text-5xl font-thin mt-24 mb-4">
+            Be free within constraints
+          </h2>
+          <h1 class="tracking-widest text-xl font-extralight mb-10">ACTRESS</h1>
+          <div
+            class="leading-loose text-sm flex flex-col items-center font-thin tracking-widest mb-16"
+          >
+            <p class="">エンタメの説明文エンタメの説明文</p>
+            <p>わからないことは、ワクワクする。</p>
+            <p>積み重ねてきた過去の経験。感情の昇華。</p>
+            <p>DETOXでもあり、新しい自分との出逢いである</p>
+            <p>俳優という仕事を通して自分自身と向き合い</p>
+            <p class="mb-8">
+              宝物を探しながら、人生をおもしろく、豊かにしていく。
+            </p>
+            <p>その人の魅力は「その人らしさ」</p>
+            <p class="mb-8">唯一無二。</p>
+            <p>誰かの「らしさ」-生き方を変えるかもしれない</p>
+            <p>魅力的な作品、世界の中を生きることで伝えていく。</p>
+          </div>
+          <nuxt-link to="/Contact" class="mb-32">
+            <button
+              class="p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700"
+            >
+              Contact
+            </button>
           </nuxt-link>
         </div>
       </section>
-      <div class="col-start-2 h-screen bg-green-200">
-        <img class="h-screen w-screen object-cover" src="~/assets/leg.jpg" />
+      <div class="col-span-2 lg:col-start-2 bg-green-200">
+        <img class="h-full w-full object-cover" src="~/assets/leg.jpg" />
       </div>
-      <div class="col-start-1 h-screen bg-green-200">
-        <img class="h-screen w-screen object-cover" src="~/assets/sun.jpg" />
+      <div class="hidden lg:contents lg:col-span-1 lg:col-start-1 bg-green-200">
+        <img class="h-full w-full object-cover" src="~/assets/sun.jpg" />
       </div>
-      <section class="col-start-2 h-screen bg-gray-200 flex justify-center items-center">
-        <div>
-          <h2>Happiness comes from within</h2>
-          <h1>COUNSELOR</h1>
-          <p>
-            「自分のこと」は自分が一番わからなかったりする。<br>
-            わからないことは、ワクワクする。<br>
-            積み重ねてきた過去の経験。感情の昇華。<br>
-            DETOXでもあり、新しい自分との出逢いである<br>
-            俳優という仕事を通して自分自身と向き合い<br>
-            宝物を探しながら、人生をおもしろく、豊かにしていく。</p>
-          <p>その人の魅力は「その人らしさ」<br>
-            唯一無二。
-          </p>
-          <p>
-            誰かの「らしさ」-生き方を変えるかもしれない<br>
-            魅力的な作品、世界の中を生きることで伝えていく。
-          </p>
-          <nuxt-link to="/Link01">
-            <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">service</button>
+      <section class="col-span-2 lg:col-span-1 lg:col-start-2 bg-gray-200 flex justify-center items-center">
+        <div class="flex flex-col items-center">
+          <h2 class="text-yellow-600 text-5xl font-thin mt-24 mb-4">
+            Happiness comes from within
+          </h2>
+          <h1 class="tracking-widest text-xl font-extralight mb-10">
+            COUNSELOR
+          </h1>
+          <div
+            class="leading-loose text-sm flex flex-col items-center font-thin tracking-widest mb-16"
+          >
+            <p class="">翻訳家の説明文翻訳家の説明文</p>
+            <p>わからないことは、ワクワクする。</p>
+            <p>積み重ねてきた過去の経験。感情の昇華。</p>
+            <p>DETOXでもあり、新しい自分との出逢いである</p>
+            <p>俳優という仕事を通して自分自身と向き合い</p>
+            <p class="mb-8">
+              宝物を探しながら、人生をおもしろく、豊かにしていく。
+            </p>
+            <p>その人の魅力は「その人らしさ」</p>
+            <p class="mb-8">唯一無二。</p>
+            <p>誰かの「らしさ」-生き方を変えるかもしれない</p>
+            <p>魅力的な作品、世界の中を生きることで伝えていく。</p>
+          </div>
+          <nuxt-link to="/Link01" class="mb-32">
+            <button
+              class="p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700"
+            >
+              service
+            </button>
           </nuxt-link>
         </div>
       </section>
-      <section class="col-start-1 h-screen bg-gray-200 flex justify-center items-center">
-        <div>
-          <h2>名前</h2>
-          <h1>name</h1>
+      <div class=" col-span-2 lg:col-start-1 lg:hidden bg-green-200">
+        <img class="h-full w-full object-cover" src="~/assets/sun.jpg" />
+      </div>
+      <section class="col-span-2 lg:col-span-1 lg:col-start-1 bg-gray-200 flex justify-center items-center">
+        <div class="flex flex-col items-center">
+          <h2 class="mb-4">華子</h2>
+          <h1 class="text-3xl tracking-widest font-semibold mb-20">
+            YAMADA HANAKO
+          </h1>
           <div>
             <nuxt-link to="/Contact">
-              <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">profile</button>
+              <button
+                class="tracking-widest text-lg p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700"
+              >
+                profile
+              </button>
             </nuxt-link>
             <nuxt-link to="/Contact">
-              <button class="border border-blue-900 p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700">biography</button>
+              <button
+                class="tracking-widest text-lg p-2 rounded text-black hover:bg-blue-900 hover:text-white transition duration-700"
+              >
+                biography
+              </button>
             </nuxt-link>
           </div>
         </div>
       </section>
-      <div class="col-start-2 h-screen bg-green-200">
-        <img class="h-screen w-screen object-cover" src="~/assets/woman2.jpg" />
+      <div class="col-span-2 lg:col-start-2 bg-green-200">
+        <img class="user-img object-cover" src="~/assets/woman2.jpg" />
       </div>
     </div>
     <div class="flex justify-center items-center h-screen">
       <div>
-        <h2>Just the way you are</h2>
-        <p>
-          自分らしくのびのびと<br>
-          表現することをやめず<br>
-          輝くその才能が<br>
-          後世に引き継がれますように
-        </p>
+        <h2 class="text-yellow-600 text-5xl font-thin mt-24 mb-12">
+          Just the way you are
+        </h2>
+        <div class="leading-loose flex flex-col items-center font-thin tracking-widest mb-16">
+          <p>自分らしくのびのびと</p>
+          <p>表現することをやめず</p>
+          <p>輝くその才能が</p>
+          <p>後世に引き継がれますように</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-
+.user-img {
+  height: 820px;
+  width: 100%;
+}
 </style>
