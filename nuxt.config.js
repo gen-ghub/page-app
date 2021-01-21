0179a767aa697dfc00bd0e60c5b9fdff1739d6fb
gen-ghub <renbenzhuang234@gmail.com>
2021-01-21 12:20:38 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,6 +56,16 @@ export default {
         .get('https://happy-shaw.microcms.io/api/v1/blog?limit=100', {
           headers: { 'X-API-KEY': '190b409a-566e-4d65-9583-e4876da6e482' },
         })
+        .then((res) =>
+          res.data.contents.map((content) => ({
+            route: `/${content.id}`,
+            payload: content,
+          }))
+        )
+      return pages
+    },
+    async routes() {
+      const pages = await axios
         .get('https://happy-shaw.microcms.io/api/v1/news?limit=100', {
           headers: { 'X-API-KEY': '190b409a-566e-4d65-9583-e4876da6e482' },
         })
